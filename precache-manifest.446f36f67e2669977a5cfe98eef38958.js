4791abd588357768c57be8cbad89c0b2616369db
chirayurathi <chirayurathi4@gmail.com>
2020-07-23 17:00:06 +0530

    Updates

---
+++ precache-manifest.446f36f67e2669977a5cfe98eef38958.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f042cf44db4a2ef9e3134ea7753d91c3",
+    "url": "/phonebook/index.html"
+  },
+  {
+    "revision": "7dd28861723228411b88",
+    "url": "/phonebook/static/css/main.42d5b287.chunk.css"
+  },
+  {
+    "revision": "4da30a21ffbf4ffcbdc8",
+    "url": "/phonebook/static/js/2.b3e5948a.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/phonebook/static/js/2.b3e5948a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dd28861723228411b88",
+    "url": "/phonebook/static/js/main.213ba752.chunk.js"
+  },
+  {
+    "revision": "d2713ab46508e69c4ab9",
+    "url": "/phonebook/static/js/runtime-main.74c37ca1.js"
+  }
+]);
